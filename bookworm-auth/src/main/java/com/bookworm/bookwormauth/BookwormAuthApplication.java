383c84d67c62d48a87b89ea0cf535719625c8cdb
GFZ\jyj94 <jyj94@golfzon.com>
2024-01-08 18:26:04 +0900

    Cloud Config 설정 수정

--- bookworm-auth/src/main/java/com/bookworm/bookwormauth/BookwormAuthApplication.java
+++ bookworm-auth/src/main/java/com/bookworm/bookwormauth/BookwormAuthApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class BookwormAuthApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BookwormAuthApplication.class, args);
     }
-
 }
