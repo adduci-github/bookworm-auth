a2f6b0e019d64e454c118c5ded1a93438d279995
GFZ\jyj94 <jyj94@golfzon.com>
2024-01-08 18:43:35 +0900

    테스트용 컴포즈 파일 추가

--- bookworm-auth/src/main/java/com/bookworm/bookwormauth/api/TestController.java
+++ bookworm-auth/src/main/java/com/bookworm/bookwormauth/api/TestController.java
@@ -1,16 +1,12 @@
 package com.bookworm.bookwormauth.api;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @Value("${test.message}")
-    private String message;
-
     @GetMapping("/test")
     public String test() {
-        return message;
+        return "Hello World!";
     }
 }
