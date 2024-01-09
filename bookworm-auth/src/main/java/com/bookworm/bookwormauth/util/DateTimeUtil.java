1620b5adc587654d6a9820c30bac65033cb92698
GFZ\jyj94 <jyj94@golfzon.com>
2024-01-09 15:44:46 +0900

    DateTimeUtil 객체 생성 불가능하도록 수정

--- bookworm-auth/src/main/java/com/bookworm/bookwormauth/util/DateTimeUtil.java
+++ bookworm-auth/src/main/java/com/bookworm/bookwormauth/util/DateTimeUtil.java
@@ -3,7 +3,9 @@ package com.bookworm.bookwormauth.util;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-public class DateTimeUtil {
+public final class DateTimeUtil {
+    private DateTimeUtil() {}
+
     public static long getCurrentEpochMilli() {
         return LocalDateTime.now().atZone(ZoneId.of("UTC")).toInstant().toEpochMilli();
     }
